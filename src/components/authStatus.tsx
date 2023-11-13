e5d9f5175eb0aa4d4002ad9904577fd642f3c20a
kicok <kicoker@gmail.com>
2023-11-13 15:09:36 +0900

    로그인 사용자 영역 변경

--- src/components/authStatus.tsx
+++ src/components/authStatus.tsx
@@ -3,6 +3,7 @@
 import { useSession, signIn, signOut } from 'next-auth/react';
 import { useEffect } from 'react';
 
+// 로그아웃
 async function keycloakSessionLogOut() {
    try {
       await fetch(`/api/auth/logout`, { method: 'GET' });
@@ -23,9 +24,10 @@ export default function AuthStatus() {
    if (status == 'loading') {
       return <div className="my-3">Loading...</div>;
    } else if (session) {
+      // 로그인 이후 사용자 정보 가져오기
       return (
-         <div className="my-3">
-            Logged in as <span className="text-yellow-100">{session.user.email}</span>{' '}
+         <div>
+            <span>{session.user.name}</span>{' '}
             <button
                className="bg-blue-900 font-bold text-white py-1 px-2 rounded border border-gray-50"
                onClick={() => {
@@ -40,7 +42,6 @@ export default function AuthStatus() {
 
    return (
       <div className="my-3">
-         Not logged in.{' '}
          <button className="bg-blue-900 font-bold text-white py-1 px-2 rounded border border-gray-50" onClick={() => signIn('keycloak')}>
             Log in
          </button>
