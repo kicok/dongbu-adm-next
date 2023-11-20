17805bf73c3b79afdeacd490c6f323604e422389
kicok <kicoker@gmail.com>
2023-11-20 10:17:30 +0900

    로그인 창 스타일 변경

--- src/components/authStatus.tsx
+++ src/components/authStatus.tsx
@@ -16,18 +16,19 @@ export default function AuthStatus() {
    const { data: session, status } = useSession();
 
    useEffect(() => {
-      if (status != 'loading' && session && session?.error === 'RefreshAccessTokenError') {
+      if (status !== 'loading' && session && session?.error === 'RefreshAccessTokenError') {
          signOut({ callbackUrl: '/' });
       }
    }, [session, status]);
 
-   if (status == 'loading') {
-      return <div className="my-3">Loading...</div>;
-   } else if (session) {
+   // if (status === 'loading') {
+   //    return <div className="ml-3">Loading...</div>;
+   // } else
+   if (session) {
       // 로그인 이후 사용자 정보 가져오기
       return (
-         <div>
-            <span>{session.user.name}</span>{' '}
+         <div className="ml-3">
+            <span>{session.user.name}</span>
             <button
                type="button"
                className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
@@ -45,7 +46,7 @@ export default function AuthStatus() {
       <div className="my-3">
          <button
             type="button"
-            className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+            className="whitespace-nowrap py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
             onClick={() => signIn('keycloak')}
          >
             로그인
